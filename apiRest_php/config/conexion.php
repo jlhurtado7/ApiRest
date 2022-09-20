8077d41aa53eef0f0f7720e27d2d0275d77e2023
joel hurtado <j.holivares@outlook.com>
2022-09-20 15:52:07 -0300

    comentario de version

--- apiRest_php/config/conexion.php
+++ apiRest_php/config/conexion.php
@@ -5,6 +5,11 @@ class Conectar{
 
     protected function Conexion(){
         try {
+            // Version de QA
+            //$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=jl_webservice","root","");
+            //Producción Godaddy
+            //$conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=jl_webservice","root","");
+            // Producción Heroku
             $conectar = $this->dbh = new PDO("mysql:local=localhost;dbname=jl_webservice","root","");
             return $conectar;
         } catch (Exception $e) {
